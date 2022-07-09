f9feea6c029e2ea90e528c3eafd01bf3f2d8dc41
Wilson Caetano <wilson@qyon.com>
2022-07-09 10:25:26 -0300

    Configure routing and connection to redis database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-export interface Env {}
+import { Router } from "worktop";
+import { listen } from "worktop/cache";
+import { Redis } from "@upstash/redis";
 
-export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    return new Response("Hello World!");
-  },
-};
+const routes = new Router();
+
+declare global {
+  var UPSTASH_REDIS_REST_URL: string;
+  var UPSTASH_REDIS_REST_TOKEN: string;
+}
+
+const redis = new Redis({
+  url: UPSTASH_REDIS_REST_URL,
+  token: UPSTASH_REDIS_REST_TOKEN,
+});
+
+routes.add("GET", "/get", async (req, res) => {
+  const access_count = await redis.get("access_count");
+
+  return res.send(200, { access_count });
+});
+
+routes.add("POST", "/increment", async (req, res) => {
+  const access_count = await redis.incr("access_count");
+
+  return res.send(201, { access_count });
+});
+
+listen(routes.run);
